f66f7efffa0bceb3ebf3cc43f123b899f45187ad
Mayank <mayanktiwari1212.mt@gmail.com>
2026-06-21 22:32:47 +0530

    feat: add AUTH_COOKIE constant and cookie config properties (secure, same-site)

--- src/main/java/tech/mayanktiwari/deployer/common/config/Constants.java
+++ src/main/java/tech/mayanktiwari/deployer/common/config/Constants.java
@@ -5,6 +5,7 @@ public final class Constants {
   public static final String USERNAME = "username";
   public static final String AUTHORIZATION = "Authorization";
   public static final String BEARER = "Bearer ";
+  public static final String AUTH_COOKIE = "auth_token";
 
   private Constants() {}
 }
